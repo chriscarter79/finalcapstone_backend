c0e22cb5be82bc8da944c884565e598de2b0496e
chriscarter79 <chris.carter@outlook.com>
2021-08-26 16:13:20 -0400

    user story 01 complete

--- src/db/seeds/00-reservations.js
+++ src/db/seeds/00-reservations.js
@@ -1,7 +1,6 @@
-const reservations = require('../seedData/00-reservations')
-
+const reservations = require('./00-reservations.json');
 exports.seed = function (knex) {
   return knex
     .raw('TRUNCATE TABLE reservations RESTART IDENTITY CASCADE')
-    .then(() => knex('reservations').insert(reservations))
-}
+    .then(() => knex('reservations').insert(reservations));
+};
--- src/reservations/reservations.router.js
+++ src/reservations/reservations.router.js
@@ -1,27 +1,19 @@
 /**
-* Defines the router for reservation resources.
-*
-* @type {Router}
-*/
-const methodNotAllowed = require('../errors/notFound')
-const router = require('express').Router()
-const controller = require('./reservations.controller')
+ * Defines the router for reservation resources.
+ *
+ * @type {Router}
+ */
 
-router
- .route('/')
- .get(controller.show)
- .post(controller.create)
- .all(methodNotAllowed)
-
-router
- .route('/:reservation_Id/status')
- .put(controller.updateStatus)
- .all(methodNotAllowed)
-
-router
- .route('/:reservation_Id')
- .get(controller.read)
- .put(controller.update)
- .all(methodNotAllowed)
-
-module.exports = router
+ const router = require('express').Router();
+ const controller = require('./reservations.controller');
+ const methodNotAllowed = require('../errors/methodNotAllowed');
+ const cors = require('cors');
+ 
+ router
+   .route('/')
+   .get(controller.list)
+   .post(controller.create)
+   .all(methodNotAllowed);
+ 
+ module.exports = router;
+ 
